de6470268eaaf7f3b484258d779811179f8facce
rCheung <rcheung1997@gmail.com>
2024-05-15 07:55:43 -0700

    separated into new page

---
+++ src/pages/SearchImages.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+
+function SearchImages() {
+
+  const [photos, setPhotos] = useState([]);
+
+  return (
+    <div className="search-images-div">
+      <h1>Image Search App</h1>
+
+      <SearchForm setPhotos={setPhotos} />
+
+      <SearchResults photos={photos} setPhotos={setPhotos} />
+    </div>
+  )
+}
+
+export default SearchImages;
