ae602ccca3249286ad2b9baec6edef13e174f647
rCheung <rcheung1997@gmail.com>
2024-05-15 08:02:44 -0700

    initialized documentation page

---
+++ src/pages/Documentation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Documentation() {
+    return (
+        <div className="documentation-div">
+            <h5>Documentation</h5>
+        </div>
+        
+    );
+}
+
+export default Documentation;
