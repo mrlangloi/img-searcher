d3f1715e512b1bc35c7052f8df2c2a0e7f7fe0bf
rCheung <rcheung1997@gmail.com>
2024-05-15 06:01:51 -0700

    included "load more" button

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,20 +7,24 @@ function SearchResults(props) {
 
 
   return (
-    <div className="search-results">
+    <div className="search-results-div">
 
-      {photos.map((photo) => {
+      <div className="search-results">
+
+        {photos.map((photo) => {
           return (
             <div className="search-result" key={photo.id}>
               <a href={photo.src.large} target="_blank">
                 <img src={photo.src.large} alt={photo.alt} />
-                {photo.alt}
+                <p>{photo.alt}</p>
               </a>
             </div>
           )
-      })}
+        })}
 
-      {/* <div className="search-result">
+
+
+        {/* <div className="search-result">
         <a href="https://www.pexels.com/photo/top-view-photo-of-ramen-soup-2591594/" target="_blank">
           <img src="https://images.pexels.com/photos/2591594/pexels-photo-2591594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Noodle soup" />
           Noodle Soup
@@ -38,6 +42,9 @@ function SearchResults(props) {
           Noodle Soup
         </a>
       </div> */}
+      </div>
+
+      <button id="load-more-btn">Load More</button>
     </div>
   );
 }
