1c88a0fa3a57146b19f4d46f2392f72090a4d5d2
rCheung <rcheung1997@gmail.com>
2024-05-16 05:57:47 -0700

    updated documentation

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -5,7 +5,12 @@ function Documentation() {
         <div className="documentation-div">
             <h1>Documentation</h1>
             <div className="doc-p-div">
-              
+              <p>
+                Search for images by typing in the search bar and pressing the "Search" button.
+              </p>
+              <p>
+                Click on an image to view it in full size.
+              </p>
             </div>
         </div>
     );
