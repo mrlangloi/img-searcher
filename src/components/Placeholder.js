e8605db01f54d78757eefde6f9d6dacdeeac5612
rCheung <rcheung1997@gmail.com>
2024-05-16 06:30:25 -0700

    now uses ImageCard.js

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -1,26 +1,42 @@
 import React from "react";
+import ImageCard from "./ImageCard";
 
 function Placeholder() {
+
+  const photos = [
+    {
+      id: 1,
+      src: {
+        large: "https://images.pexels.com/photos/2591594/pexels-photo-2591594.jpeg",
+      },
+      alt: "Top View Of Noodle soup",
+      title: "Top View Photo Of Noodle Soup"
+    },
+    {
+      id: 2,
+      src: {
+        large: "https://images.pexels.com/photos/1462725/pexels-photo-1462725.jpeg",
+      },
+      alt: "Pink and Black Nintendo DS",
+      title: "Pink And Black Nintendo DS"
+    },
+    {
+      id: 3,
+      src: {
+        large: "https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg",
+      },
+      alt: "Orange Tabby Cat Near Window",
+      title: "Orange Tabby Cat Near Window"
+    }
+  ]
+
   return (
     <div className="search-results">
-      <div className="img-card">
-        <a href="https://images.pexels.com/photos/2591594/pexels-photo-2591594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" target="_blank" rel="noreferrer">
-          <img src="https://images.pexels.com/photos/2591594/pexels-photo-2591594.jpeg" alt="Top View Of Noodle soup" />
-          <p>Top View Photo Of Noodle Soup</p>
-        </a>
-      </div>
-      <div className="img-card">
-        <a href="https://images.pexels.com/photos/1462725/pexels-photo-1462725.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" target="_blank" rel="noreferrer">
-          <img src="https://images.pexels.com/photos/1462725/pexels-photo-1462725.jpeg" alt="Pink and Black Nintendo DS" />
-          <p>Pink And Black Nintendo DS</p>
-        </a>
-      </div>
-      <div className="img-card">
-        <a href="https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" target="_blank" rel="noreferrer">
-          <img src="https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg" alt="Orange Tabby Cat Near Window" />
-          <p>Orange Tabby Cat Near Window</p>
-        </a>
-      </div>
+      {photos.map((photo) => {
+        return (
+          <ImageCard photo={photo} key={photo.id} />
+        )
+      })}
     </div>
   )
 }
