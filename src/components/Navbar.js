93cff3afa18b99452934dfc59ca7da740d8e2f86
rCheung <rcheung1997@gmail.com>
2024-05-16 05:53:35 -0700

    updated navbar to be reactive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+
+  const [active, setActive] = useState("active");
+
   return (
     <nav className="navbar">
-      <Link to="/" className="site-home">Search</Link>
+      <Link to="/" className="site-home" onClick={() => setActive("Search")}>Search</Link>
       <ul className="navbar-ul">
-        <li><Link to="/Documentation">Documentation</Link></li>
-        <li><Link to="/About">About</Link></li>
+        <li className={active === "Documentation" ? "active" : ""} onClick={() => setActive("Documentation")}>
+          <Link to="/Documentation">Documentation</Link>
+        </li>
+        <li className={active === "About" ? "active" : ""} onClick={() => setActive("About")}>
+          <Link to="/About">About</Link>
+        </li>
       </ul>
     </nav>
   );
