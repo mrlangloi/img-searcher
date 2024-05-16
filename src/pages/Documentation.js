dfdbe97782a01890f184c38232de4aeb3e085392
rCheung <rcheung1997@gmail.com>
2024-05-16 06:00:03 -0700

    fixed line spacing

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 function Documentation() {
-    return (
-        <div className="documentation-div">
-            <h1>Documentation</h1>
-            <div className="doc-p-div">
-              <p>
-                Search for images by typing in the search bar and pressing the "Search" button.
-              </p>
-              <p>
-                Click on an image to view it in full size.
-              </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="documentation-div">
+      <h1>Documentation</h1>
+      <div className="doc-p-div">
+        <p>
+          Search for images by typing in the search bar and pressing the "Search" button.
+        </p>
+        <p>
+          Click on an image to view it in full size.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Documentation;
